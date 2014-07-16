3ebca515796c6c9776b32035409318a698d52cb9
occhioDiFalco <michele.linardi@alice.it>
2014-07-16 12:28:13 +0200

    Revert "first commit assign 1 and 2"
    This reverts commit d31a79840c31e6c2d9486457c2984b9d310dae30.

--- Assign1/ex1.hpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-
-#ifndef EX1_HPP
-#define EX1_HPP
-
-std::vector<int> intersection(const std::vector<int> &a, const std::vector<int> &b);
-std::vector<int> unione(const std::vector<int> &a, const std::vector<int> &b);
-
-#endif // EX1_HPP
